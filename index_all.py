788674ed2ef544556a579ba7a65ab75ac1edd10e
saysjeong <saysjeong@gmail.com>
2026-09-24 22:30:27 +0900

    Update index_all.py

--- index_all.py
+++ index_all.py
@@ -54,7 +54,7 @@ with st.container():
     st.write("**사용 기술:** Python, Streamlit")
     st.write("Open API를 활용해 최신 개별공시지가를 확인할 수 있습니다.")
     # 프로젝트 관련 링크나 버튼을 추가할 수 있습니다.
-    st.button("프로젝트 1 상세보기", key="p1")
+    # st.button("프로젝트 1 상세보기", key="p1")
     
     st.link_button(
     label="(프로젝트 1) 개별공시지가 조회 시스템 바로가기", 
